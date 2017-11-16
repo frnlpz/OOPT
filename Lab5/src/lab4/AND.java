cdf7a663a57a5b639c9d4cd9545a75e064a03053
Fran López <fraloag@inf.upv.es>
2017-11-16 19:18:05 +0200

    Lab5 done

---
+++ Lab5/src/lab4/AND.java
@@ -0,0 +1,19 @@
+package lab4;
+import java.io.Serializable;
+
+public class AND extends Element implements Serializable {
+
+    @Override
+    public boolean getOutput() {
+        if (inputs.isEmpty()) {
+            return false;
+        } else {
+            for (Element e : inputs) {
+                if (!e.getOutput()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ Lab5/src/lab5/Lab5.java
@@ -0,0 +1,33 @@
+package lab5;
+import lab4.Schema;
+
+public class Lab5 {
+
+    public static void main(String[] args) {
+        Schema a = new Schema();
+        a.addInput(); //1
+        a.addInput(); //2
+        a.addInput(); //3
+        a.addInput(); //4
+        a.addNOT();   //5
+        a.addAND();   //6
+        a.addOR();    //7
+        a.addXOR();   //8
+
+        a.connectElements(1, 5);
+        a.connectElements(1, 6);
+        a.connectElements(2, 6);
+        a.connectElements(3, 6);
+        a.connectElements(3, 7);
+        a.connectElements(4, 7);
+        a.connectElements(5, 8);
+        a.connectElements(6, 8);
+        a.connectElements(7, 8);
+
+        a.setFalse(1);
+        a.setTrue(2);
+        a.setTrue(3);
+        a.setFalse(4);
+        a.printElement(8);
+    }
+}
